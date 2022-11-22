0ab4b0e8a39d2b35a273f87d09a579c478cc6a72
IlyasOussama <zghariilyasoussama@gmail.com>
2022-11-22 22:34:36 +0100

    api

--- src/pages/api/api.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
-
-const API_URL = "https://api.lens.dev";
-
-/* create the API client */
-export const client = new ApolloClient({
-  uri: API_URL,
-  cache: new InMemoryCache(),
-});
