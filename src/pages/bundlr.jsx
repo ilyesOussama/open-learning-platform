8abc634fd7d1be13e6231d61e6a96a1c9f026e7d
IlyasOussama <zghariilyasoussama@gmail.com>
2022-11-22 22:33:09 +0100

    bundlr page

---
+++ src/pages/bundlr.jsx
@@ -0,0 +1,52 @@
+import { WebBundlr } from "@bundlr-network/client";
+import { MainContext } from "../context";
+import { useState, useRef } from "react";
+import { providers, utils } from "ethers";
+
+import BundlrComp from "../components/BundlrComp";
+
+const Bundlr = () => {
+  const [bundlrInstance, setBundlrInstance] = useState();
+  const [balance, setBalance] = useState();
+  const bundlrRef = useRef();
+  async function initialiseBundlr() {
+    await window.ethereum.enable();
+
+    const provider = new providers.Web3Provider(window.ethereum);
+    await provider._ready();
+
+    const bundlr = new WebBundlr(
+      "https://node1.bundlr.network",
+      "matic",
+      provider
+    );
+    await bundlr.ready();
+
+    setBundlrInstance(bundlr);
+    bundlrRef.current = bundlr;
+    fetchBalance();
+  }
+
+  async function fetchBalance() {
+    const bal = await bundlrRef.current.getLoadedBalance();
+    console.log("bal: ", utils.formatEther(bal.toString()));
+    setBalance(utils.formatEther(bal.toString()));
+  }
+
+  return (
+    <div>
+      <MainContext.Provider
+        value={{
+          initialiseBundlr,
+          bundlrInstance,
+          balance,
+          fetchBalance,
+        }}
+      >
+        <BundlrComp />
+      </MainContext.Provider>
+    </div>
+  );
+};
+
+export default Bundlr;
